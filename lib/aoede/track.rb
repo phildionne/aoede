13bcc9eb0a81c231f9498b3108a52fcef89942b7
Philippe Dionne <dionne.phil@gmail.com>
2014-02-11 12:23:36 -0500

    Add Track filename attribute

--- lib/aoede/track.rb
+++ lib/aoede/track.rb
@@ -1,5 +1,12 @@
 module Aoede
 
   class Track
+    attr_accessor :filename
+
+    # @param filename [String]
+    def filename=(filename)
+      raise ArgumentError, "No such file: #{filename}" unless File.exist?(filename)
+      @filename = filename
+    end
   end
 end
