1fe1b73c3a347c8ea48536939c8ba285c752a86f
Philippe Dionne <dionne.phil@gmail.com>
2014-02-11 12:24:50 -0500

    Add Track audio attribute

--- lib/aoede/track.rb
+++ lib/aoede/track.rb
@@ -17,5 +17,18 @@ module Aoede
       raise ArgumentError, "No such file: #{filename}" unless File.exist?(filename)
       @filename = filename
     end
+
+    # @return [TagLib::FileRef, TagLib::MP4::File, TagLib::MPEG::File, TagLib::FLAC::File, TagLib::OGG::Vorbis::File]
+    def audio
+      @audio ||= case filename
+                 # Do not read audio properties for faster initialization
+                 # see http://rubydoc.info/gems/taglib-ruby/TagLib/FileRef:initialize
+                 when /\.mp4\z/  then ::TagLib::MP4::File.new(filename, false)
+                 when /\.mp3\z/  then ::TagLib::MPEG::File.new(filename, false)
+                 when /\.flac\z/ then ::TagLib::FLAC::File.new(filename, false)
+                 when /\.oga\z/  then ::TagLib::OGG::Vorbis::File.new(filename, false)
+                 else ::TagLib::FileRef.new(filename, false)
+                 end
+    end
   end
 end
