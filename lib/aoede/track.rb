e95b576c333e64a38bb3c7399fb5d2ab888a4093
Philippe Dionne <dionne.phil@gmail.com>
2014-02-11 12:24:08 -0500

    Allow hash instantiation in Track#new

--- lib/aoede/track.rb
+++ lib/aoede/track.rb
@@ -3,6 +3,15 @@ module Aoede
   class Track
     attr_accessor :filename
 
+    # @param attributes [Hash]
+    def initialize(attributes = {})
+      attributes.each do |attr, value|
+        self.public_send("#{attr}=", value)
+      end
+
+      self
+    end
+
     # @param filename [String]
     def filename=(filename)
       raise ArgumentError, "No such file: #{filename}" unless File.exist?(filename)
