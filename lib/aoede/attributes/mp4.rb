375593aff665670105f4362e0c1a47a626c73ceb
Philippe Dionne <dionne.phil@gmail.com>
2014-02-12 21:36:28 -0500

    Add Attributes::MP4 attribute setters

--- lib/aoede/attributes/mp4.rb
+++ lib/aoede/attributes/mp4.rb
@@ -7,48 +7,65 @@ module Aoede
       include Aoede::Attributes::Base
 
       ATTRIBUTES = {
-        album: "©alb",
-        album_artist: "aART",
-        artist: "©art",
-        artwork: "covr",
-        bpm: "tmpo",
-        category: "catg",
-        comment: "©cmt",
-        compilation: "cpil",
-        composer: "©wrt",
-        copyright: "cprt",
-        description: "desc",
-        disk_number: "disk",
-        encoder: "©too",
-        episode_global_unique_id: "egid",
-        gapless_playback: "pgap",
-        genre: "©gen",
-        grouping: "©grp",
-        keyword: "keyw",
-        lyrics: "©lyr",
-        podcast: "pcst",
-        podcast_url: "purl",
-        purchase_date: "purd",
-        rating_advisory: "rtng",
-        sort_album: "soal",
-        sort_album_artist: "soaa",
-        sort_artist: "soar",
-        sort_composer: "soco",
-        sort_title: "sonm",
-        stik: "stik",
-        title: "©nam",
-        track_number: "trkn",
-        tv_episode: "tves",
-        tv_episode_number: "tven",
-        tv_network_name: "tvnn",
-        tv_season: "tvsn",
-        tv_show_name: "tvsh",
-        year: "©day"
+        album: '©alb',
+        album_artist: 'aART', # FIXME
+        artist: '©art',       # FIXME
+        artwork: 'covr',
+        bpm: 'tmpo',
+        category: 'catg',
+        comment: '©cmt',
+        compilation: 'cpil',
+        composer: '©wrt',
+        copyright: 'cprt',
+        description: 'desc',
+        disk_number: 'disk',
+        encoder: '©too',
+        episode_global_unique_id: 'egid',
+        gapless_playback: 'pgap',
+        genre: '©gen',
+        grouping: '©grp',
+        keyword: 'keyw',
+        lyrics: '©lyr',
+        podcast: 'pcst',
+        podcast_url: 'purl',
+        purchase_date: 'purd',
+        rating_advisory: 'rtng',
+        sort_album: 'soal',
+        sort_album_artist: 'soaa',
+        sort_artist: 'soar',
+        sort_composer: 'soco',
+        sort_title: 'sonm',
+        stik: 'stik',
+        title: '©nam',
+        track_number: 'trkn',
+        tv_episode: 'tves',
+        tv_episode_number: 'tven',
+        tv_network_name: 'tvnn',
+        tv_season: 'tvsn',
+        tv_show_name: 'tvsh',
+        year: '©day'
       }
 
-      # @param audio [TagLib::MP4::File]
+      included do
+        define_attribute_getters
+        define_attribute_setters
+      end
+
+      module ClassMethods
+
+        def define_attribute_setters
+          ATTRIBUTES.keys.each do |method|
+            send(:define_method, "#{method}=") do |value|
+              audio.tag.item_list_map.insert(ATTRIBUTES[method], ::TagLib::MP4::Item.from_string_list([value]))
+            end
+          end
+        end
+
+        private :define_attribute_setters
+      end
+
       # @return [Hash]
-      def self.attributes(audio)
+      def attributes
         attrs = Hash.new
 
         ATTRIBUTES.each do |key, value|
