4d10b9bed4ce47130f513e78290bda9c8a07ee00
Philippe Dionne <dionne.phil@gmail.com>
2014-02-17 16:20:31 -0500

    Visual reorder

--- lib/aoede/track.rb
+++ lib/aoede/track.rb
@@ -34,9 +34,9 @@ module Aoede
                  # Do not read audio properties for faster initialization
                  # see http://rubydoc.info/gems/taglib-ruby/TagLib/FileRef:initialize
                  when /\.(mp4|m4a|m4p|m4b|m4r|m4v)\z/ then ::TagLib::MP4::File.new(filename, false)
-                 when /\.mp3\z/  then ::TagLib::MPEG::File.new(filename, false)
-                 when /\.flac\z/ then ::TagLib::FLAC::File.new(filename, false)
                  when /\.(oga|ogg)\z/ then ::TagLib::Ogg::Vorbis::File.new(filename, false)
+                 when /\.flac\z/      then ::TagLib::FLAC::File.new(filename, false)
+                 when /\.mp3\z/       then ::TagLib::MPEG::File.new(filename, false)
                  else ::TagLib::FileRef.new(filename, false)
                  end
     end
