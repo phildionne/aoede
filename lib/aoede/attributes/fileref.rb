09a8d817ef43db6edf001450db9afa5b679b568d
Philippe Dionne <dionne.phil@gmail.com>
2014-02-19 16:43:30 -0500

    Remove deprecated logic

--- lib/aoede/attributes/fileref.rb
+++ lib/aoede/attributes/fileref.rb
@@ -4,20 +4,6 @@ module Aoede
   module Attributes
     module FileRef
       include Aoede::Attributes::Base
-
-      ATTRIBUTES = [:album, :artist, :comment, :genre, :title, :track, :year]
-
-      # @return [Hash]
-      def attributes
-        attrs = Hash.new
-
-        ATTRIBUTES.each do |key|
-          attrs[key] = audio.tag.send(value)
-        end
-
-        attrs
-      end
-
     end
   end
 end
